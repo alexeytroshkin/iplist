3f29851d55ada8f352b73ca212afb3ec2e30ca38
Alexey Troshkin <alextroshkin@outlook.com>
2026-06-28 16:08:25 +0300

    Separate origin and merged; preserve origin as downloaded

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ set -e
 
 curl -fsSL 'https://iplist.opencck.org/?format=text&data=cidr4' -o origin
 
-sort -u origin <(sed 's/#.*//; s/[[:space:]]*$//' added) | grep -v -F -x -f removed | sed '/^$/d' > origin.tmp
-mv origin.tmp origin
+sort -u origin <(sed 's/#.*//; s/[[:space:]]*$//' added) | grep -v -F -x -f removed | sed '/^$/d' > merged
